8b9b0346b99c953576a50710b9d4c0076fb80343
Karim Suleymanov <gchdjjfjfjf@mail.ru>
2024-10-24 09:18:26 +0300

    откат

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 import express from 'express'
 import router from "./router/index.js";
-import * as https from "https";
-import * as fs from "fs";
 
 const PORT = 5000
 
@@ -13,10 +11,7 @@ app.use('/api', router)
 
 async function startApp() {
     try {
-        https.createServer({
-            key: fs.readFileSync('/cert/key.pem'),
-            cert: fs.readFileSync('/cert/cert.pem')
-        }, app).listen(PORT, () => {
+        app.listen(PORT, () => {
             console.log("SERVER STARTED ON PORT " + PORT)
         })
     } catch (e) {
